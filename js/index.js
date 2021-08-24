b804be52b562ab2b2d3858d564cad54bb782738e
vitallyz <vitally.zilber@gmail.com>
2021-08-24 22:22:12 +1000

    Moved moving cells functionality to the Cell class. Added option to move 2 or 3 cells in one click. Counting moves

--- js/index.js
+++ js/index.js
@@ -1,154 +1,186 @@
-const cells = [];
 
-const cellWithNumberColour = "#e9c111";
-const cellEmptyColour = "#d4d4d4";
+const thePuzzle15Game = (function () {
+    const cells = [];
+    let totalMovesPlayed = 0;
 
-let emptyCell;
+    const cellWithNumberColour = "#e9c111";
+    const cellEmptyColour = "#d4d4d4";
 
-class Cell {
-    #id;
-    #value;
-    #element;
-    #isEmpty;
+    let emptyCell;
 
-    constructor (id, element) {
-        this.#id = id;
-        (id === 15) ? this.#value = "": this.#value = id + 1;
+    class Cell {
+        #id;
+        #element;
 
-        if (id === 15) {
-            this.#isEmpty = true;
-            emptyCell = id;
+        constructor (id, element) {
+            this.#id = id;
+            this.#element = element;
+
+            if (id === 15) {
+                // this.#value = "";
+                emptyCell = id;
+                this.#element.style.background = cellEmptyColour;
+                this.#element.style.color = "white";
+                this.#element.innerText = "";
+            } else {
+                this.#element.style.background = cellWithNumberColour;
+                this.#element.style.color = "white";
+                this.#element.innerText = id + 1;
+                // this.#value = id + 1;
+            }
+
+           
         }
 
-        this.#element = element;
-    }
-
-    get id() {
-        return this.#id;
-    }
-
-    get value() {
-        return this.#value;
-    }
-
-    get element() {
-        return this.#element;
-    }
-    set value(value) {
-        if (value >= 0 && value <= 15) {
-            this.#value = value;
+        get id() {
+            return this.#id;
         }
-    } 
 
-    clicked() {
+        get element() {
+            return this.#element;
+        }
 
-    }
-}
+        clicked() {
+            // Lets check if clicked on empty cell
+            if (this.#id === emptyCell) {
+                console.log("Clicked on empty cell");
+                return;
+            }
 
-// initializing array of Cell class instances 
-for (let id = 0; id < 16; id++) {
-    const element = document.getElementById(`cell_${id}`);
-    cells[id] = new Cell(id, element);
+            // Lets check if we have an empty space above us
+            if ((this.#id - 4 === emptyCell)) {
+                const newId = this.#id - 4;
+                this.swapCells(newId);
+                return;  
+            }
 
-}
+            if ((this.#id - 8 === emptyCell)) {
+                let newId = this.#id - 8;
+                cells[this.#id - 4].swapCells(newId);
+                newId = this.#id - 4;
+                this.swapCells(newId);
+                return;  
+            }
 
-// Render on screen initial state
-for (let id = 0; id < 16; id++) {
-    if (id !== 15) {
-        console.log(cells[id].element.style.background);
-        cells[id].element.style.background = cellWithNumberColour;
-        cells[id].element.style.color = "white";
-        cells[id].element.innerText = cells[id].value;
-    } else {
-        cells[id].element.style.background = cellEmptyColour;
-        cells[id].element.style.color = "white";
-        cells[id].element.innerText = cells[id].value;
-    }
+            if ((this.#id - 12 === emptyCell)) {
+                let newId = this.#id - 12;
+                cells[this.#id - 8].swapCells(newId);
+                newId = this.#id - 8;
+                cells[this.#id - 4].swapCells(newId);
+                newId = this.#id - 4;
+                this.swapCells(newId);
+                return;  
+            }
+
+            // Lets check if we have an empty space below us
+            if (this.#id + 4 === emptyCell) {
+                const newId = this.#id + 4;
+                this.swapCells(newId);
+                return;
+            }
+
+            if ((this.#id + 8 === emptyCell)) {
+                let newId = this.#id + 8;
+                cells[this.#id + 4].swapCells(newId);
+                newId = this.#id + 4;
+                this.swapCells(newId);
+                return;  
+            }
+
+            if ((this.#id + 12 === emptyCell)) {
+                let newId = this.#id + 12;
+                cells[this.#id + 8].swapCells(newId);
+                newId = this.#id + 8;
+                cells[this.#id + 4].swapCells(newId);
+                newId = this.#id + 4;
+                this.swapCells(newId);
+                return;  
+            }
     
-}
-
-// Add event listeners to all cells in the table
-for (let id = 0; id < 16; id++) {
-    cells[id].element.addEventListener("click", clickHandler);
-}
-
-function clickHandler(element) {
-    //console.log("Registered click on element", element.target);
-    const id = parseInt((element.target.id.split("_")[1]), 10);
-    // console.log("Our id is ", id);
-    // Lets check if we have an empty space above us
-    if (id === emptyCell) {
-        console.log("Clicked on empty cell");
-        return;
-    }
+            // Checking if the clicked cell is on the same line as the empty cell
+            
+            if (parseInt(this.#id / 4, 10) === parseInt(emptyCell / 4, 10)) {
     
-    if ((id - 4 === emptyCell)) {
-        let newId = id - 4;
-        cells[newId].element.innerText = cells[id].element.innerText;
-        cells[id].element.innerText = "";
-        cells[id].element.style.background = cellEmptyColour;
-        cells[newId].element.style.background = cellWithNumberColour;
+                // Lets check if we have an empty cell on the right
+                if (this.#id + 1 === emptyCell) {
+                    const newId = this.#id + 1;
+                    this.swapCells(newId);
+                    return;
+                }
+                
+                if (this.#id + 2 === emptyCell) {
+                    let newId = this.#id + 2;
+                    cells[this.#id + 1].swapCells(newId);
+                    newId = this.#id + 1;
+                    this.swapCells(newId);
+                    return;
+                }
 
-        emptyCell = id;
-        console.log("new empty cell is ", emptyCell);
-        console.log("We have empty cell above!");
-        return;
-        
-    }
+                if (this.#id + 3 === emptyCell) {
+                    let newId = this.#id + 3;
+                    cells[this.#id + 2].swapCells(newId);
+                    newId = this.#id + 2;
+                    cells[this.#id + 1].swapCells(newId);
+                    newId = this.#id + 1;
+                    this.swapCells(newId);
+                    return;
+                }
 
-    // Lets check if we have an empty space below us
-    if (id + 4 === emptyCell) {
-        console.log("We have empty cell below!");
-        let newId = id + 4;
-        cells[newId].element.innerText = cells[id].element.innerText;
-        cells[id].element.innerText = "";
-        cells[id].element.style.background = cellEmptyColour;
-        cells[newId].element.style.background = cellWithNumberColour;
-        
-        emptyCell = id;
-        console.log("new empty cell is ", emptyCell);
-        return;
-    }
+                // Lets check if there is an empty cell on the Left
+                if (this.#id - 1 === emptyCell) {
+                    const newId = this.#id - 1;
+                    this.swapCells(newId);
+                    return;   
+                }
 
-    // Checking if the clicked cell is on the same line as the empty cell
-    
-    if (parseInt(id / 4, 10) === parseInt(emptyCell / 4, 10)) {
+                if (this.#id - 2 === emptyCell) {
+                    let newId = this.#id - 2;
+                    cells[this.#id - 1].swapCells(newId);
+                    newId = this.#id - 1;
+                    this.swapCells(newId);
+                }
 
-        console.log ("Emtpty cell and clicked cell are on the same line");
-        // Lets check if we have an empty cell on the right
-        if (id + 1 === emptyCell) {
-            const newId = id + 1;
-            cells[newId].element.innerText = cells[id].element.innerText;
-            cells[id].element.innerText = "";
-            cells[id].element.style.background = cellEmptyColour;
+                if (this.#id - 3 === emptyCell) {
+                    let newId = this.#id - 3;
+                    cells[this.#id - 2].swapCells(newId);
+                    newId = this.#id - 2;
+                    cells[this.#id - 1].swapCells(newId);
+                    newId = this.#id - 1;
+                    this.swapCells(newId);
+                    return;
+                }
+            }
+
+        }
+
+        swapCells (newId) {
+            cells[newId].element.innerText = this.#element.innerText;
+            this.#element.innerText = "";
+            this.#element.style.background = cellEmptyColour;
             cells[newId].element.style.background = cellWithNumberColour;
-            
-            emptyCell = id;
-            console.log("new empty cell is ", emptyCell);
-            return;
-            
-        }
-
-        // Lets check if there is an empty cell on the Left
-        if (id - 1 === emptyCell) {
-            const newId = id - 1;
-            cells[newId].element.innerText = cells[id].element.innerText;
-            cells[id].element.innerText = "";
-            cells[id].element.style.background = cellEmptyColour;
-            cells[newId].element.style.background = cellWithNumberColour;
-            
-            emptyCell = id;
-            console.log("new empty cell is ", emptyCell);
-            return;
-            
+            emptyCell = this.#id;
+            totalMovesPlayed++;
+            console.log(totalMovesPlayed);
         }
     }
 
+    // initializing array of Cell class instances 
+    for (let id = 0; id < 16; id++) {
+        const element = document.getElementById(`cell_${id}`);
+        cells[id] = new Cell(id, element);
+    }
 
 
-    // console.log("parseInt(id + 4, 10) is ", parseInt(id + 4, 10))
-    // console.log("Our empty cell is ", emptyCell)
-    
+    // Add event listeners to all cells in the table
+    for (let id = 0; id < 16; id++) {
+        cells[id].element.addEventListener("click", clickHandler);
+    }
 
-}
+    function clickHandler(element) {
+        const id = parseInt((element.target.id.split("_")[1]), 10);
+        cells[id].clicked();
+    }
+
+})(document);
+
 
