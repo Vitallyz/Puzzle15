887cdd9af34ffd99159a9b089b39d953754614ea
vitallyz <vitally.zilber@gmail.com>
2021-08-30 22:21:06 +1000

    changed cell size to 80px

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const thePuzzle15Game = (function () {
     
     
     const cells = [];
-    const cellSize = 90;
+    const cellSize = 80;
     const cellSizeInternal = cellSize - 3;
     const tableSize =  cellSize * 4
     let modalView = "";
